ebed3fefcfc88e296cd6e875efc0f659e19716f2
danielroman11 <danielroman.05111@gmail.com>
2023-09-27 01:22:07 -0500

    style(card):fix(newArticle): Style cards & article

--- components/index.js
+++ components/index.js
@@ -2,26 +2,38 @@ export function newArticle(id, name, salary ){
   let article = document.createElement("article");
   
   //* Elements
+  //* --> Data
   const idE = document.createElement("p");
-  const nameE = document.createElement("h3");
+  const nameE = document.createElement("h4");
   const salaryE = document.createElement("p");
+  //* --> Containers
   const headerE = document.createElement("header");
+  const footerE = document.createElement("footer");
+  const sectionE = document.createElement("section");
+  //* --> Buttons
   const editE = document.createElement("button");
   const deleteE = document.createElement("button");
   
   //* Texts
   idE.appendChild(document.createTextNode(`${id}`));
-  nameE.appendChild(document.createTextNode(`Name: ${name}`));
-  salaryE.appendChild(document.createTextNode(`Salary: ${salary}`));
-  editE.textContent("Edit");
-  deleteE.textContent("Delete");
+  nameE.appendChild(document.createTextNode(`${name}`));
+  salaryE.appendChild(document.createTextNode(`Earn $${salary}`));
+  editE.appendChild(document.createTextNode("Edit"));
+  deleteE.appendChild(document.createTextNode("Delete"));
 
   //* Styles
   article.classList.add("card");
+  headerE.classList.add("header");
+  footerE.classList.add("footer")
+  sectionE.classList.add("section");
+
   idE.classList.add("id");
 
   //* AppendChilds
-  article.append(idE, headerE);
+  sectionE.append(nameE, salaryE);
+  headerE.append(idE, sectionE);
+  footerE.append(editE, deleteE);
+  article.append(headerE, footerE);
 
 
   return article;
